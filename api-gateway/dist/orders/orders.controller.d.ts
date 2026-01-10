483abf989ce876faec388e4a8dfdd4d35ec18e4d
mcca09 <lexy.xsbr@gmail.com>
2026-01-10 18:57:29 -0400

    Revert "Creación de estructuras"

--- api-gateway/dist/orders/orders.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ClientProxy } from '@nestjs/microservices';
-export declare class OrdersController {
-    private readonly ordersClient;
-    constructor(ordersClient: ClientProxy);
-    createOrder(createOrderDto: any, req: any): Promise<import("rxjs").Observable<any>>;
-    getMyOrders(req: any): Promise<import("rxjs").Observable<any>>;
-}
--- api-gateway/dist/stalls/stalls.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ClientProxy } from '@nestjs/microservices';
-export declare class StallsController {
-    private readonly stallsClient;
-    constructor(stallsClient: ClientProxy);
-    create(createStallDto: any, req: any): import("rxjs").Observable<any>;
-    findAll(): import("rxjs").Observable<any>;
-}
